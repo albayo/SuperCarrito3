5c734d5e00c379884b695132872cd23db4762bf0
japerego <japerego@unirioja.es>
2021-05-04 17:30:28 +0200

    Arreglado mostrar productos 2da vez

--- app/src/main/java/com/example/accesoconcorreo/TodosProductos.java
+++ app/src/main/java/com/example/accesoconcorreo/TodosProductos.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.firebase.database.DataSnapshot;
@@ -23,6 +24,7 @@ import ModeloDominio.Producto;
 
 /**
  * Esta clase define la actividad llamada "activity_productos_compra" que sirve para mostrar todos los productos de la base de datos.
+ *
  * @author: Pablo Ochoa, Javier Pérez, Marcos Moreno, Álvaro Bayo
  * @version: 30/04/2021
  */
@@ -37,20 +39,21 @@ public class TodosProductos extends AppCompatActivity {
     /**
      * Método que sirve para inicializar y cargar todos los elementos visuales de la actividad
      * "activity_productos_comprar"
+     *
      * @param savedInstanceState Representa el objeto donde se guarda la información
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todos_productos);
-        String nombreLista=getIntent().getStringExtra("nombreLista");
-        idLista=getIntent().getStringExtra("idLista");
-        recyclerViewproductos=(RecyclerView)findViewById(R.id.super_prod_list_recycler);
+        String nombreLista = getIntent().getStringExtra("nombreLista");
+        idLista = getIntent().getStringExtra("idLista");
+        recyclerViewproductos = (RecyclerView) findViewById(R.id.super_prod_list_recycler);
         recyclerViewproductos.setLayoutManager(new LinearLayoutManager(this));
-        toolbar=findViewById(R.id.toolbar_lista_super_prod);
+        toolbar = findViewById(R.id.toolbar_lista_super_prod);
         toolbar.setTitle(nombreLista);
-        mDatabase= FirebaseDatabase.getInstance().getReference();
-        productos=new ArrayList<>();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        productos = new ArrayList<>();
         obtenerTodosProductos();
     }
 
@@ -59,12 +62,12 @@ public class TodosProductos extends AppCompatActivity {
      * y darle la opción de añadirlos a su lista.
      */
     public void obtenerTodosProductos() {
-
+        productos.clear();
         mDatabase.child("json").child("results").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (snapshot.exists()) {
-                    Iterable<DataSnapshot> dataSnapshots=snapshot.getChildren();
+                    Iterable<DataSnapshot> dataSnapshots = snapshot.getChildren();
                     /*for(int i=0;i<40;i++){
                         String id=dataSnapshots.iterator().next().getKey();
                         Log.d("ID",id);
@@ -73,29 +76,34 @@ public class TodosProductos extends AppCompatActivity {
 
                     }*/
                     for (DataSnapshot ds : snapshot.getChildren()) {
-                        String id=ds.getKey();
-                        if(productos.size()<5){
-                            addProducto(id);
-                        }
+                        String id = ds.getKey();
+                        String nombre = ds.child("product_name").getValue().toString();
+                        String ingredients = ds.child("ingredients_text").getValue().toString();
+                        String imgage = ds.child("image_url").getValue().toString();
+                        String brand = ds.child("brand_owner").getValue().toString();
+                        String gradoNutricion = ds.child("nutriscore_grade").getValue().toString();
+                        Producto p = new Producto(id, nombre, brand, "", ingredients, "Eroski", gradoNutricion);
+                        productos.add(p);
 
                     }
-                    todosProductosAdapter= new TodosProductosAdapter(TodosProductos.this,R.layout.item_productos_comprar,productos,idLista);
+                    todosProductosAdapter = new TodosProductosAdapter(TodosProductos.this, R.layout.item_productos_comprar, productos, idLista);
                     recyclerViewproductos.setAdapter(todosProductosAdapter);
                 }
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
             }
-        }
+                                                                       }
         );
 
     }
 
     /**
      * Método que añade un producto con id "idProducto" a la lsita del usuario en la que se está
+     *
      * @param idProducto Representa el id del producto que quieres añadir a la lista.
      */
-    public void addProducto(String idProducto){
+    public void addProducto(String idProducto) {
 
 
         mDatabase.child("json").child("results").child(idProducto).addValueEventListener(new ValueEventListener() {
@@ -106,18 +114,17 @@ public class TodosProductos extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot ds) {
                 if (ds.exists()) {
-                    String id=ds.getKey();
+                    String id = ds.getKey();
                     String nombre = ds.child("product_name").getValue().toString();
                     String ingredients = ds.child("ingredients_text").getValue().toString();
                     String imgage = ds.child("image_url").getValue().toString();
                     String brand = ds.child("brand_owner").getValue().toString();
-                    String gradoNutricion= ds.child("nutriscore_grade").getValue().toString();
-                    Producto p = new Producto(idProducto, nombre, brand, "", ingredients, "Eroski",gradoNutricion);
+                    String gradoNutricion = ds.child("nutriscore_grade").getValue().toString();
+                    Producto p = new Producto(idProducto, nombre, brand, "", ingredients, "Eroski", gradoNutricion);
                     productos.add(p);
 
 
-                        todosProductosAdapter.setProductos(productos);
-
+                    todosProductosAdapter.setProductos(productos);
 
 
                 }
