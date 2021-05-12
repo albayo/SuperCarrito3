4dc89ceaa44b4f4a3be2bf6ae31436d22eca71dd
mamorecab <56037340+mamorecab@users.noreply.github.com>
2021-05-12 15:51:48 +0200

    adapter solicitudes hecho

--- app/src/main/java/Adapters/SolicitudesAdapter.java
+++ app/src/main/java/Adapters/SolicitudesAdapter.java
@@ -1,11 +1,17 @@
 package Adapters;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.accesoconcorreo.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +21,7 @@ import ModeloDominio.Solicitud;
 public class SolicitudesAdapter extends RecyclerView.Adapter<SolicitudesAdapter.SolicitudViewHolder> {
 
     //No puede ser de lista de Strings
-    private List<Solicitud> mSolicitudes=new ArrayList<>();
+    private List<Solicitud> mSolicitudes;
     private Activity activity;
     private int resource;
     private String nick;
@@ -31,23 +37,62 @@ public class SolicitudesAdapter extends RecyclerView.Adapter<SolicitudesAdapter.
     @NonNull
     @Override
     public SolicitudViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(resource,parent,false);
+        return new SolicitudesAdapter.SolicitudViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull SolicitudViewHolder holder, int position) {
+        if(mSolicitudes!=null){
+            Solicitud current = mSolicitudes.get(position);
+
+            holder.remitente.setText(current.getRemitente());
+            if(current.getTipoSolicitud().equals("amistad"))
+                holder.tipoSolicitud.setText("Solicitud de amistad de:");
+            else
+                holder.tipoSolicitud.setText("Solicitud de participar en la lista "+current.getNombreLista()+" de:");
+        }
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if(mSolicitudes!=null)
+            return mSolicitudes.size();
+        else
+            return 0;
+
     }
+
+
 
     public class SolicitudViewHolder extends  RecyclerView.ViewHolder{
 
+        private final EditText tipoSolicitud, remitente;
+        private final ImageButton aceptar, declinar;
+
         public SolicitudViewHolder(@NonNull View itemView) {
             super(itemView);
+            tipoSolicitud=itemView.findViewById(R.id.tipo_solicitud);
+            remitente=itemView.findViewById(R.id.remitente);
+            aceptar=itemView.findViewById(R.id.aceptar_sol);
+            declinar=itemView.findViewById(R.id.declinar_sol);
+
+            aceptar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            declinar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
         }
+
+
     }
 }
