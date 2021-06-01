390df1f10468aa83cdcfc71432a798e5f376d732
albayo <albayo@unirioja.es>
2021-06-01 18:58:17 +0200

    Apaños

--- app/src/main/java/com/example/accesoconcorreo/ficha_producto.java
+++ app/src/main/java/com/example/accesoconcorreo/ficha_producto.java
@@ -1,24 +1,16 @@
 package com.example.accesoconcorreo;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.view.menu.MenuView;
 import androidx.appcompat.widget.Toolbar;
 
-import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
-import Adapters.ProductListAdapter;
 import ModeloDominio.Producto;
 
 import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade;
@@ -43,6 +35,13 @@ public class ficha_producto extends AppCompatActivity {
     //Representa la marca del producto que se dispondrá por pantalla
     private TextView brand;
 
+    //Representa el precio del producto que se dispondrá por pantalla
+    private TextView precio;
+
+    //Representa el super del producto que se dispondrá por pantalla
+    private TextView superm;
+    //Representa la categoría del producto que se dispondrá por pantalla
+    private TextView categoria;
     /**
      * Método que sirve para inicializar y cargar todos los elementos visuales de la actividad
      * "activity_ficha_producto"
@@ -65,10 +64,21 @@ public class ficha_producto extends AppCompatActivity {
 
         fotoprod= findViewById(R.id.imagen_ficha_producto);
         fotonutri= findViewById(R.id.image_grado_nutrition);
-        nutricional= findViewById(R.id.textview_nutrientes);
+
         nombre= findViewById(R.id.text_nombre_producto);
         brand=findViewById(R.id.text_brand_prod);
-
+        precio=findViewById(R.id.tv_nombre_precio);
+        superm=findViewById(R.id.tv_nombre_super);
+        categoria=findViewById(R.id.tv_nombre_categoria);
+        if(producto.getCategoria()!=null){
+            categoria.setText(producto.getCategoria());
+        }
+        if(producto.getSupermercado()!=null){
+            superm.setText(producto.getSupermercado());
+        }
+        if(producto.getPrecio()!=null){
+            precio.setText(producto.getPrecio()+"€");
+        }
         String n=producto.getNombre();
         if(n.contains(","))
             nombre.setText(n.split(",")[1]);
@@ -82,17 +92,18 @@ public class ficha_producto extends AppCompatActivity {
 
         else
             fotoprod.setImageResource(R.mipmap.pordefecto);
-        
-        String nutri=producto.getGradoNutrition();
+
+
+       /* String nutri=producto.getGradoNutrition();
         switch(nutri){
             case "a":fotonutri.setImageResource(R.mipmap.nutriscore_a);
             case "b":fotonutri.setImageResource(R.mipmap.nutriscore_b);
             case "c":fotonutri.setImageResource(R.mipmap.nutriscore_c);
             case "d":fotonutri.setImageResource(R.mipmap.nutriscore_d);
             case "e":fotonutri.setImageResource(R.mipmap.nutriscore_e);
-        }
+        }*/
 
-       nutricional.setText("ejemplo");
+
 
         brand.setText(producto.getBrand());
     }
